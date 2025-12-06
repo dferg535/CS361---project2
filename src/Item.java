304a8cef28c7099344ccbbf9dfa564c94635ba6c
Dustin Ferguson <dferg535@gmail.com>
2025-12-06 12:15:25 -0700

    Added class for item.

---
+++ src/Item.java
@@ -0,0 +1,20 @@
+
+/**
+ * An item or element used in the Knapsack Problem. Each item has a ID, weight,
+ * value, and priority factor.
+ */
+public class Item {
+    
+    int ID;
+    double weight;
+    double value;
+    double priorityFactor;
+    
+    public Item (int ID, double weight, double value, double priorityFactor) {
+        this.ID = ID;
+        this.weight = weight;
+        this.value = value;
+        this.priorityFactor = priorityFactor;
+    }
+    
+}
