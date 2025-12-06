23866b9e4c8132197e7a54332cf8ec22844ac078
Nathan <ntjmw6657@gmail.com>
2025-12-06 12:50:31 -0700

    Added the add method for Max Binary Heap and added to package

---
+++ src/Project2/MaxBinHeap.java
@@ -0,0 +1,42 @@
+package Project2;
+
+public class MaxBinHeap {
+
+    public MaxBinHeap(Item[] array) {
+        Item[] newArray = new Item[array.length];
+    }
+
+    public Item[] add(Item newItem, Item[] array) {
+
+        //Keep track of position that item was replaced at
+        int ArraySpot = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == null) {
+                //Adding new item to next available position
+                array[i] = newItem;
+                //Save this to compare with parent in HeapSort
+                ArraySpot = i;
+                break;
+            }
+        }
+
+        //Compare to parent
+
+        int ParentSpot = (ArraySpot - 1) / 2;
+        while(ParentSpot > -1) {
+            Item parentNode = array[ParentSpot];
+
+            //If the Parent node is less than the Child, we need to swap for max binary heap
+            if(parentNode.priorityFactor < newItem.priorityFactor) {
+                array[ParentSpot] = newItem;
+                array[ArraySpot] = parentNode;
+                ParentSpot = (ParentSpot - 1) / 2;
+            }
+
+        }
+
+
+        return array;
+    }
+
+}
